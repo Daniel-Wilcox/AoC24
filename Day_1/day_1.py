094e7fb355bf68ac1ea7f09b52e66baad1e5f040
Dan <d.dt.wilcox@gmail.com>
2024-12-02 16:03:30 +0200

    completed day 1

--- Day_1/day_1.py
+++ Day_1/day_1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-def main():
-
+def part_1():
     df = pd.read_csv("Day_1/day_1_input.csv", header=None)
     df = df.rename(columns={0:"left_id", 1:"right_id"})
     
@@ -17,7 +16,31 @@ def main():
 
     print(combined_df.head())
     print(combined_df["diff"].sum())
+    
+
+def part_2():
+    df = pd.read_csv("Day_1/day_1_input.csv", header=None)
+    df = df.rename(columns={0:"left_id", 1:"right_id"})
+
+    left_df = df[["left_id"]].copy()
+    left_df = left_df.sort_values(by="left_id", ascending=True).reset_index(drop=True)
+
+    right_df = df[["right_id"]].copy()
+    right_df = right_df.sort_values(by="right_id", ascending=True).reset_index(drop=True)
+    right_appeared_df = right_df.groupby(by="right_id").size().reset_index(name='counts')
+    right_appeared_df = right_appeared_df.rename(columns={"right_id":"left_id"})
+
+
+    combined_df = pd.merge(left_df, right_appeared_df, how="inner")
+
+    combined_df["multiplied"] = combined_df["left_id"] * combined_df["counts"]
+
+
+    print(combined_df.head())
+    print(combined_df["multiplied"].sum()) #23981443
+
+
     pass
 
 if __name__ == "__main__":
-    main()
+    part_2()

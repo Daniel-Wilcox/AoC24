cbb1424c6f839e1096e7160dbb69c7e043b6904c
Dan <d.dt.wilcox@gmail.com>
2024-12-02 15:26:49 +0200

    Added project init CLI script for the provided day

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Advent of Code 2024
+
+This year I have decided to participate in the [Advent of Code](https://adventofcode.com/2024/about) challenge. I'll attempt to be consistent throughout the challenges, despite missing the first day 🫠. 
+
+## Helper script
+
+I've created a script called [generate_project_day.py](./generate_project_day.py) which can be ran in a CLI to provide a basic initialization for the day's challenge. Currently it just achieves the following:
+
+- Create "Day_X" directory
+- touch "day_X_puzzle.txt"
+- touch "day_X_input.csv"
+- create python file: "day_X.py"
+
+where the "day_X.py" contains a basic initial script:
+
+```python 
+#Day_X/day_X.py
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+```
+
+To run this from the CLI, one can use the following command:
+
+```bash
+python generate_project_day.py 7
+```
+
+which will create the above mentioned folders and files for the 7th day. 
---
+++ generate_project_day.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+def _touch_file(folder_name: str, file_name: str):
+    file = os.path.join(folder_name, file_name)
+    with open(file, 'w') as f:
+        pass
+
+
+def _create_python_file(folder_name: str, file_name: str):
+    py_file = os.path.join(folder_name, file_name)
+    with open(py_file, 'w') as f:
+        f.write(
+            f"""def main():\n    pass\n\nif __name__ == "__main__":\n    main()\n"""
+        )
+
+def create_project_folder(day):
+    folder_name = f"Day_{day}"
+    
+    # Creating folders and files
+    os.makedirs(folder_name, exist_ok=True)
+    _touch_file(folder_name, f"day_{day}_puzzle.txt")
+    _touch_file(folder_name, f"day_{day}_input.csv")
+    _create_python_file(folder_name, f"day_{day}.py")
+
+
+if __name__ == "__main__":
+
+    # Create the parser requirements
+    parser = argparse.ArgumentParser()
+    parser.add_argument('day', help="Day number to use for the project folder")    
+    args = parser.parse_args()
+    
+    # Use the provided argument directly as the day variable
+    day = args.day
+    create_project_folder(day)
+

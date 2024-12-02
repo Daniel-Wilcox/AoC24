71b46fbe415ef5d2e30e35f354dee7e2f5385370
Dan <d.dt.wilcox@gmail.com>
2024-12-02 23:22:29 +0200

    completed day 2

--- Day_2/day_2.py
+++ Day_2/day_2.py
@@ -1,6 +1,3 @@
-import pandas as pd 
-
-
 def _strictly_ascending(x: list) -> bool:
 
     len_x = len(x)
@@ -13,23 +10,21 @@ def _strictly_ascending(x: list) -> bool:
     return True
 
 
-
 def check_requirement_1(x: list) -> bool:
     """The levels are either all increasing or all decreasing."""
-    
+    temp = x.copy()
     # Check strictly ascending
-    if _strictly_ascending(x):
+    if _strictly_ascending(temp):
         return True
     
     # Check strictly descending
-    x.reverse()
-    if _strictly_ascending(x):
+    temp.reverse()
+    if _strictly_ascending(temp):
         return True
     
     return False
 
     
-
 def check_requirement_2(x: list, min_diff:int = 1, max_diff:int=3) -> bool:
     """Any two adjacent levels differ by at least one and at most three."""
 
@@ -42,40 +37,83 @@ def check_requirement_2(x: list, min_diff:int = 1, max_diff:int=3) -> bool:
     return all([True if (i >= min_diff) and (i <= max_diff) else False for i in difference_list])
     
 
+def combined_requirements(x: list) -> bool:
+
+    if not check_requirement_1(x):
+        return False
+
+    if not check_requirement_2(x):
+        return False
+    
+    return True
+
 
 def part_1():
+
     filepath = "Day_2/day_2_input.csv"
-    num_safe = 0
 
     with open(filepath, "r+") as f:
-        for idx, line in enumerate(f.readlines()):
 
-            line_list = [int(i) for i in line.strip().split(",")]
+        full_list = [
+            (idx, [int(i) for i in line.strip().split(",")]) 
+            for idx, line in enumerate(f.readlines())
+        ]
 
-            if not check_requirement_1(line_list):
-                print(f"{idx}: {line_list} is unsafe (1)")
-                continue
+    full_list_safe = [
+        (idx, line_list, combined_requirements(line_list))
+        for idx, line_list in full_list
+    ]
 
-            if not check_requirement_2(line_list):
-                print(f"{idx}: {line_list} is unsafe (2)")
-                continue
-            
-            num_safe += 1
+    counting = sum([v for _, _, v in full_list_safe])
 
-            print(f"{idx}: {line_list} is Safe")
+    print(f"Total safe: {counting}")
 
-    print(f"{num_safe}") #379
-            
-    # df = pd.read_csv(
-    #     "Day_2/day_2_input.csv", 
-    #     names=["a", "b", "c", "d", "e", "f", "g", "h"]
-    # )
 
-    # print(df.head())
+def recheck_with_removed_element(x: list)->bool:
+
+    return any([
+        combined_requirements(x[:idx] + x[idx + 1:])
+        for idx in range(len(x))
+    ])
+
 
 def part_2():
-    pass
+
+    filepath = "Day_2/day_2_input.csv"
+
+
+    with open(filepath, "r+") as f:
+
+        full_list = [
+            (idx, [int(i) for i in line.strip().split(",")]) 
+            for idx, line in enumerate(f.readlines())
+        ]
+
+    full_list_safe = [
+        (idx, line_list, combined_requirements(line_list))
+        for idx, line_list in full_list
+    ]
+
+    part_1_count = sum([v for _, _, v in full_list_safe])
+
+    # Part 2 recheck
+    only_unsafe = [
+        (i, j) for (i, j, k) in full_list_safe if not k
+    ]
+
+    only_unsafe_rechecked = [
+        (idx, line_list, recheck_with_removed_element(line_list)) 
+        for idx, line_list in only_unsafe
+    ]
+    part_2_count = sum([v for _, _, v in only_unsafe_rechecked])
+
+    print(f"{part_2_count = }") # 379 < X < 442?
+
+    counting = part_1_count + part_2_count
+
+    print(f"Total safe: {counting}") # 430
+
 
 if __name__ == "__main__":
-    part_1()
-    # part_2()
+    # part_1()
+    part_2()
